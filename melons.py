33549f0bc6c75fe3bb39ca610d3bb8e4f2346b10
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-12 12:59:53 -0800

    Created abstract, domestic, international, and governmental classes

---
+++ melons.py
@@ -0,0 +1,73 @@
+"""Classes for melon orders."""
+
+from random import randint
+
+
+class AbstractMelonOrder(object):
+    """Abstract class for melon orders"""
+
+    shipped = False
+
+    def __init__(self, species, qty):
+        self.species = species
+        self.qty = qty
+
+    def get_base_price(self):
+        if not self.base_price:
+            self.base_price = randint(5, 9)
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        self.get_base_price()
+        print base_price
+        if self.species == "Christmas melon":
+            new_base_price = 1.5 * self.base_price
+
+        total = (1 + self.tax) * self.qty * new_base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+
+    order_type = "domestic"
+    tax = 0.08
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    order_type = "international"
+    tax = 0.17
+
+    def __init__(self, species, qty, country_code):
+        super(InternationalMelonOrder, self).__init__(species, qty)
+        self.country_code = country_code
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+    def get_total(self):
+        total = super(InternationalMelonOrder, self).get_total()
+        if self.qty < 10:
+            total += 3
+
+        return total
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+
+    tax = 0.00
+    passed_inspection = False
+
+    def mark_inspection(self, passed):
+
+        self.passed_inspection = passed
+
